ba55c391ed4bbab772732f233b3e12008bea9915
fdussert <fdussert@gmail.com>
2010-12-15 15:47:32 +0000

    __toString Refactoring for fields
    git-svn-id: https://svn.mageekbox.net/repositories/atoum/trunk@188 fe8bb9cb-3c6a-df11-91c9-004063e5cbdf

--- classes/reports/cli.php
+++ classes/reports/cli.php
@@ -11,20 +11,20 @@ class cli extends atoum\report
 		parent::__construct();
 
 		$this
-			->addRunnerField(new atoum\report\fields\runner\version(), array(atoum\runner::runStart))
-			->addRunnerField(new atoum\report\fields\runner\tests\duration(), array(atoum\runner::runStop))
-			->addRunnerField(new atoum\report\fields\runner\tests\memory(), array(atoum\runner::runStop))
-			->addRunnerField(new atoum\report\fields\runner\tests\coverage(), array(atoum\runner::runStop))
+			->addRunnerField(new atoum\report\fields\runner\version\string(), array(atoum\runner::runStart))
+			->addRunnerField(new atoum\report\fields\runner\tests\duration\string(), array(atoum\runner::runStop))
+			->addRunnerField(new atoum\report\fields\runner\tests\memory\string(), array(atoum\runner::runStop))
+			->addRunnerField(new atoum\report\fields\runner\tests\coverage\string(), array(atoum\runner::runStop))
 			->addRunnerField(new atoum\report\fields\runner\duration\string(), array(atoum\runner::runStop))
-			->addRunnerField(new atoum\report\fields\runner\result(), array(atoum\runner::runStop))
-			->addRunnerField(new atoum\report\fields\runner\failures(), array(atoum\runner::runStop))
-			->addRunnerField(new atoum\report\fields\runner\outputs(), array(atoum\runner::runStop))
-			->addRunnerField(new atoum\report\fields\runner\errors(), array(atoum\runner::runStop))
-			->addRunnerField(new atoum\report\fields\runner\exceptions(), array(atoum\runner::runStop))
-			->addTestField(new atoum\report\fields\test\run(), array(atoum\test::runStart))
-			->addTestField(new atoum\report\fields\test\event())
-			->addTestField(new atoum\report\fields\test\duration(), array(atoum\test::runStop))
-			->addTestField(new atoum\report\fields\test\memory(), array(atoum\test::runStop))
+			->addRunnerField(new atoum\report\fields\runner\result\string(), array(atoum\runner::runStop))
+			->addRunnerField(new atoum\report\fields\runner\failures\string(), array(atoum\runner::runStop))
+			->addRunnerField(new atoum\report\fields\runner\outputs\string(), array(atoum\runner::runStop))
+			->addRunnerField(new atoum\report\fields\runner\errors\string(), array(atoum\runner::runStop))
+			->addRunnerField(new atoum\report\fields\runner\exceptions\string(), array(atoum\runner::runStop))
+			->addTestField(new atoum\report\fields\test\run\string(), array(atoum\test::runStart))
+			->addTestField(new atoum\report\fields\test\event\string())
+			->addTestField(new atoum\report\fields\test\duration\string(), array(atoum\test::runStop))
+			->addTestField(new atoum\report\fields\test\memory\string(), array(atoum\test::runStop))
 		;
 
 		$stringDecorator = new atoum\report\decorators\string();
