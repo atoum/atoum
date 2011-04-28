e08f5628de1ab1dfc5df67fdedd095d39cfde17c
mageekguy <frederic.hardy@mageekbox.net>
2011-04-28 11:58:20 +0000

    Improve tagger.
    git-svn-id: https://svn.mageekbox.net/repositories/atoum/trunk@400 fe8bb9cb-3c6a-df11-91c9-004063e5cbdf

--- classes/scripts/tagger.php
+++ classes/scripts/tagger.php
@@ -9,26 +9,90 @@ use
 
 class tagger extends atoum\script
 {
-	protected $sourceDirectory = null;
+	const versionTag = '<tagger:version />';
 
-	public function getSourceDirectory()
+	protected $srcDirectory = null;
+	protected $destinationDirectory = null;
+	protected $srcIteratorInjector = null;
+
+	public function getSrcDirectory()
 	{
-		return $this->sourceDirectory;
+		return $this->srcDirectory;
 	}
 
-	public function setSourceDirectory($directory)
+	public function setSrcDirectory($directory)
 	{
-		$this->sourceDirectory = (string) $directory;
+		$this->srcDirectory = (string) $directory;
+
+		if ($this->destinationDirectory === null)
+		{
+			$this->destinationDirectory = $this->srcDirectory;
+		}
 
 		return $this;
 	}
 
-	public function getFilesIterator()
+	public function getDestinationDirectory()
 	{
-		if ($this->sourceDirectory === null)
+		return $this->destinationDirectory;
+	}
+
+	public function setDestinationDirectory($directory)
+	{
+		$this->destinationDirectory = (string) $directory;
+
+		return $this;
+	}
+
+	public function setSrcIteratorInjector(\closure $srcIteratorInjector)
+	{
+		$closure = new \reflectionMethod($srcIteratorInjector, '__invoke');
+
+		if ($closure->getNumberOfParameters() != 1)
+		{
+			throw new exceptions\logic('Src iterator injector must take one argument');
+		}
+
+		$this->srcIteratorInjector = $srcIteratorInjector;
+
+		return $this;
+	}
+
+	public function getSrcIterator()
+	{
+		if ($this->srcDirectory === null)
 		{
 			throw new exceptions\logic('Unable to get files iterator, source directory is undefined');
 		}
+
+		if ($this->srcIteratorInjector === null)
+		{
+			$this->setSrcIteratorInjector(function ($directory) { return new \recursiveIteratorIterator(new atoum\src\iterator\filter(new \recursiveDirectoryIterator($directory))); });
+		}
+
+		return $this->srcIteratorInjector->__invoke($this->srcDirectory);
+	}
+
+	public function tagVersion($version)
+	{
+		if ($this->srcDirectory === null)
+		{
+			throw new exceptions\logic('Unable to tag, src directory is undefined');
+		}
+
+		$srcIterator = $this->getSrcIterator();
+
+		if ($srcIterator instanceof \iterator === false)
+		{
+			throw new exceptions\logic('Unable to tag, src iterator injector does not return an iterator');
+		}
+
+		foreach ($srcIterator as $path)
+		{
+			$this->adapter->file_put_contents($path, str_replace(self::versionTag, $version, $this->adapter->file_get_contents($path)));
+		}
+
+		return $this;
 	}
 }
 
--- tests/units/classes/scripts/tagger.php
+++ tests/units/classes/scripts/tagger.php
@@ -15,7 +15,10 @@ class tagger extends atoum\test
 	public function testClass()
 	{
 		$this->assert
-			->testedClass->isSubclassOf('\mageekguy\atoum\script')
+			->testedClass
+				->isSubclassOf('\mageekguy\atoum\script')
+				->hasInterface('\mageekguy\atoum\adapter\aggregator')
+			->string(\mageekguy\atoum\scripts\tagger::versionTag)->isEqualTo('<tagger:version />')
 		;
 	}
 
@@ -24,33 +27,122 @@ class tagger extends atoum\test
 		$tagger = new scripts\tagger(uniqid());
 
 		$this->assert
-			->variable($tagger->getSourceDirectory())->isNull()
+			->variable($tagger->getSrcDirectory())->isNull()
+			->variable($tagger->getDestinationDirectory())->isNull()
 		;
 	}
 
-	public function testSetSourceDirectory()
+	public function testSetSrcDirectory()
 	{
 		$tagger = new scripts\tagger(uniqid());
 
 		$this->assert
-			->object($tagger->setSourceDirectory($directory = uniqid()))->isIdenticalTo($tagger)
-			->string($tagger->getSourceDirectory())->isEqualTo($directory)
-			->object($tagger->setSourceDirectory($directory = rand(- PHP_INT_MAX, PHP_INT_MAX)))->isIdenticalTo($tagger)
-			->string($tagger->getSourceDirectory())->isEqualTo((string) $directory)
+			->object($tagger->setSrcDirectory($directory = uniqid()))->isIdenticalTo($tagger)
+			->string($tagger->getSrcDirectory())->isEqualTo($directory)
+			->string($tagger->getDestinationDirectory())->isEqualTo($directory)
+			->object($tagger->setSrcDirectory($otherDirectory = rand(- PHP_INT_MAX, PHP_INT_MAX)))->isIdenticalTo($tagger)
+			->string($tagger->getSrcDirectory())->isEqualTo((string) $otherDirectory)
+			->string($tagger->getDestinationDirectory())->isEqualTo($directory)
 		;
 	}
 
-	public function testGetFilesIterator()
+	public function testSetDestinationDirectory()
+	{
+		$tagger = new scripts\tagger(uniqid());
+
+		$this->assert
+			->object($tagger->setDestinationDirectory($directory = uniqid()))->isIdenticalTo($tagger)
+			->string($tagger->getDestinationDirectory())->isEqualTo($directory)
+			->object($tagger->setDestinationDirectory($directory = rand(- PHP_INT_MAX, PHP_INT_MAX)))->isIdenticalTo($tagger)
+			->string($tagger->getDestinationDirectory())->isEqualTo((string) $directory)
+		;
+	}
+
+	public function testSetSrcIteratorInjector()
+	{
+		$tagger = new scripts\tagger(uniqid());
+
+		$tagger->setSrcDirectory(__DIR__);
+
+		$this->assert
+			->exception(function() use ($tagger) {
+					$tagger->setSrcIteratorInjector(function() {});
+				}
+			)
+				->isInstanceOf('\mageekguy\atoum\exceptions\logic')
+				->hasMessage('Src iterator injector must take one argument')
+			->object($tagger->setSrcIteratorInjector(function($directory) { return new \recursiveDirectoryIterator($directory); }))->isIdenticalTo($tagger)
+			->object($tagger->getSrcIterator())->isInstanceOf('\recursiveDirectoryIterator')
+			->string($tagger->getSrcIterator()->getPath())->isEqualTo(__DIR__)
+		;
+	}
+
+	public function testGetSrcIterator()
 	{
 		$tagger = new scripts\tagger(uniqid());
 
 		$this->assert
 			->exception(function() use ($tagger) {
-					$tagger->getFilesIterator();
+					$tagger->getSrcIterator();
 				}
 			)
 				->isInstanceOf('\mageekguy\atoum\exceptions\logic')
 				->hasMessage('Unable to get files iterator, source directory is undefined')
+		;
+
+		$tagger->setSrcDirectory(__DIR__);
+
+		$this->assert
+			->object($tagger->getSrcIterator())->isInstanceOf('\recursiveIteratorIterator')
+			->object($tagger->getSrcIterator()->getInnerIterator())->isInstanceOf('\mageekguy\atoum\src\iterator\filter')
+		;
+	}
+
+	public function testTagVersion()
+	{
+		$tagger = new scripts\tagger(uniqid(), null, $adapter = new atoum\test\adapter());
+
+		$this->assert
+			->exception(function() use ($tagger) {
+					$tagger->tagVersion(uniqid());
+				}
+			)
+				->isInstanceOf('\mageekguy\atoum\exceptions\logic')
+				->hasMessage('Unable to tag, src directory is undefined')
+		;
+
+		$tagger
+			->setSrcDirectory(uniqid())
+			->setSrcIteratorInjector(function($directory) {})
+		;
+
+		$this->assert
+			->exception(function() use ($tagger) {
+					$tagger->tagVersion(uniqid());
+				}
+			)
+				->isInstanceOf('\mageekguy\atoum\exceptions\logic')
+				->hasMessage('Unable to tag, src iterator injector does not return an iterator')
+		;
+
+		$srcIterator = new \arrayIterator(array($file1 = uniqid(), $file2 = uniqid(), $file3 = uniqid()));
+
+		$tagger->setSrcIteratorInjector(function($directory) use ($srcIterator) { return $srcIterator; });
+
+		$adapter->file_get_contents[1] = ($file1Part1 = uniqid()) . \mageekguy\atoum\scripts\tagger::versionTag . ($file1Part2 = uniqid());
+		$adapter->file_get_contents[2] = $contentOfFile2 = uniqid();
+		$adapter->file_get_contents[3] = ($file3Part1 = uniqid()) . \mageekguy\atoum\scripts\tagger::versionTag . ($file3Part2 = uniqid());
+		$adapter->file_put_contents = function() {};
+
+		$this->assert
+			->object($tagger->tagVersion($version = uniqid()))->isIdenticalTo($tagger)
+			->adapter($adapter)
+				->call('file_get_contents', array($file1))
+				->call('file_put_contents', array($file1, $file1Part1 . $version . $file1Part2))
+				->call('file_get_contents', array($file2))
+				->call('file_put_contents', array($file2, $contentOfFile2))
+				->call('file_get_contents', array($file3))
+				->call('file_put_contents', array($file3, $file3Part1 . $version . $file3Part2))
 		;
 	}
 }
