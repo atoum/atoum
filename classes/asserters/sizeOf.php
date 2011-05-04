d32d11a9c2e55290dd956c71dd7ba607e6995ba0
mageekguy <frederic.hardy@mageekbox.net>
2011-05-04 20:54:13 +0000

    rename asserters/sizeof.php to asserters/sizeOf.php.
    git-svn-id: https://svn.mageekbox.net/repositories/atoum/trunk@415 fe8bb9cb-3c6a-df11-91c9-004063e5cbdf

--- classes/asserters/sizeOf.php
+++ classes/asserters/sizeOf.php
@@ -2,7 +2,7 @@
 
 namespace mageekguy\atoum\asserters;
 
-class sizeof extends \mageekguy\atoum\asserters\integer
+class sizeOf extends \mageekguy\atoum\asserters\integer
 {
 	public function setWith($variable, $label = null)
 	{
