97c97705f6538ba333f51dc4bd8997a673400c32
mageekguy <frederic.hardy@mageekbox.net>
2011-06-22 19:32:58 +0000

    Add mysqlDateTimeAsserter.
    git-svn-id: https://svn.mageekbox.net/repositories/atoum/trunk@496 fe8bb9cb-3c6a-df11-91c9-004063e5cbdf

---
+++ classes/asserters/mysqlDateTime.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace mageekguy\atoum\asserters;
+
+use
+	\mageekguy\atoum\asserters,
+	\mageekguy\atoum\exceptions
+;
+
+class mysqlDateTime extends asserters\dateTime
+{
+	const mysqlDateTimeFormat = 'Y-m-d H:i:s';
+
+	public function setWith($value, $checkType = true)
+	{
+		$phpDate = \dateTime::createFromFormat(self::mysqlDateTimeFormat, $value);
+
+		if ($phpDate !== false)
+		{
+			parent::setWith($phpDate, $checkType);
+		}
+		else
+		{
+			parent::setWith($value, false);
+
+			if ($checkType === true)
+			{
+				$this->fail(sprintf($this->getLocale()->_('%s is not in format Y-m-d H:i:s'), $this));
+			}
+		}
+
+		return $this;
+	}
+
+	public function getValue()
+	{
+		$value = parent::getValue();
+
+		return ($value instanceof \dateTime === false ? $value : $value->format(self::mysqlDateTimeFormat));
+	}
+}
+
+?>
