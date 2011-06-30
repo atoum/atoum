237de8a3b66bc782ee379445df6a53673d503245
mageekguy <frederic.hardy@mageekbox.net>
2011-06-30 23:13:01 +0200

    Start of tokenizer refactoring.

--- classes/php/tokenizer.php
+++ classes/php/tokenizer.php
@@ -12,6 +12,9 @@ class tokenizer implements \iteratorAggregate
 {
 	protected $iterator = null;
 
+	private $tokens = null;
+	private $currentIterator = null;
+
 	public function __construct()
 	{
 		$this->resetIterator();
@@ -31,271 +34,156 @@ class tokenizer implements \iteratorAggregate
 
 	public function tokenize($string)
 	{
-		$currentClass = null;
-		$currentProperty = null;
-		$currentFunction = null;
-		$currentMethod = null;
-		$currentAbstractMethod = null;
-		$currentArgument = null;
-		$currentConstant = null;
-		$currentNamespace = null;
-		$currentNamespaceImportation = null;
-		$currentDefaultValue = null;
-		$currentIterator = $this->iterator;
+		$this->currentIterator = $this->iterator;
 
-		foreach ($tokens = new \arrayIterator(token_get_all($string)) as $key => $token)
+		foreach ($this->tokens = new \arrayIterator(token_get_all($string)) as $key => $token)
 		{
 			switch ($token[0])
 			{
 				case T_USE:
-					$currentIterator->appendNamespaceImportation($currentNamespaceImportation = new iterators\phpNamespace\importation());
-					$currentIterator = $currentNamespaceImportation;
+					$token = $this->appendNamespaceImportation();
 					break;
 
 				case T_NAMESPACE:
-					if ($currentNamespace !== null)
-					{
-						$currentIterator = $currentNamespace->getParent();
-						$currentNamespace = null;
-					}
-
-					$currentIterator->appendNamespace($currentNamespace = new iterators\phpNamespace());
-					$currentIterator = $currentNamespace;
-					break;
-
-				case T_ABSTRACT:
-					if ($currentClass === null)
-					{
-						$currentIterator->appendClass($currentClass = new iterators\phpClass());
-						$currentIterator = $currentClass;
-					}
-					else
-					{
-						$currentIterator->appendMethod($currentAbstractMethod = new iterators\phpMethod());
-						$currentIterator = $currentAbstractMethod;
-					}
-					break;
-
-				case T_FINAL:
-					if ($currentClass === null)
-					{
-						$currentIterator->appendClass($currentClass = new iterators\phpClass());
-					}
-						$currentIterator = $currentClass;
-					break;
-
-				case T_CLASS:
-					if ($currentClass === null)
-					{
-						$currentIterator->appendClass($currentClass = new iterators\phpClass());
-						$currentIterator = $currentClass;
-					}
-					break;
-
-				case T_VARIABLE:
-					if ($currentMethod !== null && $currentArgument === null)
-					{
-						$currentIterator->appendArgument($currentArgument = new iterators\phpArgument());
-						$currentIterator = $currentArgument;
-					}
-					else if ($currentClass !== null && $currentProperty === null)
-					{
-						$currentIterator->appendProperty($currentProperty = new iterators\phpProperty());
-						$currentIterator = $currentProperty;
-					}
-					break;
-
-				case T_CONST:
-					$currentIterator->appendConstant($currentConstant = new iterators\phpConstant());
-					$currentIterator = $currentConstant;
-					break;
-
-				case T_FUNCTION:
-					if ($currentClass !== null && $currentMethod === null && $currentAbstractMethod === null)
-					{
-						$currentIterator->appendMethod($currentMethod = new iterators\phpMethod());
-						$currentIterator = $currentMethod;
-					}
-					else if ($currentMethod === null && $currentAbstractMethod === null)
-					{
-						$currentIterator->appendFunction($currentFunction = new iterators\phpFunction());
-						$currentIterator = $currentFunction;
-					}
-					break;
-
-				case T_PUBLIC:
-				case T_PRIVATE:
-				case T_PROTECTED:
-					if ($currentClass !== null)
-					{
-						if (self::nextTokenIs(T_FUNCTION, $tokens) === true)
-						{
-							$currentIterator->appendMethod($currentMethod = new iterators\phpMethod());
-							$currentIterator = $currentMethod;
-						}
-						else
-						{
-							$currentIterator->appendProperty($currentProperty = new iterators\phpProperty());
-							$currentIterator = $currentProperty;
-						}
-					}
-					break;
-
-				case ';':
-					if ($currentNamespaceImportation !== null)
-					{
-						$currentIterator = $currentNamespaceImportation->getParent();
-						$currentConstant = null;
-					}
-					else if ($currentAbstractMethod !== null)
-					{
-						$currentIterator = $currentAbstractMethod->getParent();
-						$currentAbstractMethod = null;
-					}
-					else if ($currentConstant !== null)
-					{
-						$currentIterator = $currentConstant->getParent();
-						$currentConstant = null;
-					}
-					else if ($currentProperty !== null)
-					{
-						$currentIterator = $currentProperty->getParent();
-						$currentProperty = null;
-					}
-					break;
-
-				case ',':
-					if ($currentDefaultValue !== null)
-					{
-						$currentIterator = $currentDefaultValue->getParent();
-						$currentDefaultValue = null;
-					}
-					else if ($currentArgument !== null)
-					{
-						$currentIterator = $currentArgument->getParent();
-						$currentArgument = null;
-					}
-					else if ($currentProperty !== null)
-					{
-						$currentIterator = $currentProperty->getParent();
-						$currentProperty = null;
-					}
-					break;
-
-				case ')':
-					if ($currentArgument !== null)
-					{
-						$currentIterator = $currentArgument->getParent();
-						$currentArgument = null;
-					}
-					break;
-
-				case T_CLOSE_TAG:
-					if ($currentNamespace !== null)
-					{
-						if (self::nextTokenIs(T_OPEN_TAG, $tokens) === false)
-						{
-							$currentIterator = $currentNamespace->getParent();
-							$currentNamespace = null;
-						}
-					}
+					$token = $this->appendNamespace();
 					break;
 			}
 
-			$currentIterator->append(new tokenizer\token($token[0], isset($token[1]) === false ? null : $token[1], isset($token[2]) === false ? null : $token[2]));
-
-			switch ($token[0])
-			{
-				case '}':
-					if ($currentMethod !== null)
-					{
-						$currentIterator = $currentMethod->getParent();
-						$currentMethod = null;
-					}
-					else if ($currentClass !== null)
-					{
-						$currentIterator = $currentClass->getParent();
-						$currentClass = null;
-					}
-					else if ($currentFunction !== null)
-					{
-						$currentIterator = $currentFunction->getParent();
-						$currentFunction = null;
-					}
-					break;
-
-				case '=':
-					if ($currentArgument !== null)
-					{
-						self::appendCommentAndWhitespace($currentIterator, $tokens);
-
-						$currentIterator->appendDefaultValue($currentDefaultValue = new iterators\phpDefaultValue());
-						$currentIterator = $currentDefaultValue;
-					}
-					break;
-
-				case T_ARRAY:
-					self::appendArray($currentIterator, $tokens);
-					break;
-			}
+			$this->currentIterator->append(new tokenizer\token($token[0], isset($token[1]) === false ? null : $token[1], isset($token[2]) === false ? null : $token[2]));
 		}
 
 		return $this;
 	}
 
-	protected static function appendCommentAndWhitespace(tokenizer\iterator $iterator, \arrayIterator $tokens)
+	private function appendNamespaceImportation()
 	{
-		$key = $tokens->key();
+		$this->currentIterator->appendNamespaceImportation($this->currentNamespaceImportation = new iterators\phpNamespace\importation());
+		$this->currentIterator = $this->currentNamespaceImportation;
 
-		while (isset($tokens[$key + 1]) === true && ($tokens[$key + 1][0] === T_WHITESPACE || $tokens[$key + 1][0] === T_COMMENT))
+		$inNamespaceImportation = true;
+
+		while ($inNamespaceImportation === true && $this->tokens->valid() === true)
 		{
-			$tokens->next();
+			$token = $this->tokens->current();
 
-			$token = $tokens->current();
+			switch ($token[0])
+			{
+				case ';':
+				case ',':
+					$this->currentIterator = $this->currentNamespaceImportation->getParent();
+					$this->currentNamespaceImportation = null;
+					$inNamespaceImportation = false;
+					break;
 
-			$iterator->append(new tokenizer\token($token[0], isset($token[1]) === false ? null : $token[1], isset($token[2]) === false ? null : $token[2]));
+				default:
+					$this->currentIterator->append(new tokenizer\token($token[0], isset($token[1]) === false ? null : $token[1], isset($token[2]) === false ? null : $token[2]));
+					$this->tokens->next();
+			}
+		}
 
-			$key = $tokens->key();
+		return $this->tokens->valid() === false ? null : $this->tokens->current();
+	}
+
+	private function appendNamespace()
+	{
+		$parent = $this->currentIterator->getParent();
+
+		if ($parent !== null)
+		{
+			$this->currentIterator = $parent;
+		}
+
+		$this->currentIterator->appendNamespace($this->currentNamespace = new iterators\phpNamespace());
+		$this->currentIterator = $this->currentNamespace;
+
+		$inNamespace = true;
+
+		while ($inNamespace === true && $this->tokens->valid() === true)
+		{
+			$token = $this->tokens->current();
+
+			switch ($token[0])
+			{
+				case T_CONST:
+					$this->appendConstant();
+					break;
+
+				case T_FUNCTION:
+					$this->appendFunction();
+					break;
+
+				case T_FINAL:
+				case T_ABSTRACT:
+				case T_CLASS:
+					$this->appendClass();
+					break;
+
+				case T_INTERFACE:
+					$this->appendInterface();
+					break;
+
+				case T_CLOSE_TAG:
+					if ($this->nextTokenIs(T_OPEN_TAG) === false)
+					{
+						$this->currentIterator = $this->currentIterator->getParent();
+						$inNamespace = false;
+					}
+					break;
+
+				case '}':
+					$this->currentIterator = $this->currentIterator->getParent();
+					$inNamespace = false;
+					break;
+			}
+
+			$this->currentIterator->append(new tokenizer\token($token[0], isset($token[1]) === false ? null : $token[1], isset($token[2]) === false ? null : $token[2]));
+
+			$this->tokens->next();
+		}
+
+		return $this->tokens->valid() === false ? null : $this->tokens->current();
+	}
+
+	private function appendCommentAndWhitespace()
+	{
+		$key = $this->tokens->key();
+
+		while (isset($this->tokens[$key + 1]) === true && ($this->tokens[$key + 1][0] === T_WHITESPACE || $this->tokens[$key + 1][0] === T_COMMENT))
+		{
+			$this->tokens->next();
+
+			$token = $this->tokens->current();
+
+			$this->currentIterator->append(new tokenizer\token($token[0], isset($token[1]) === false ? null : $token[1], isset($token[2]) === false ? null : $token[2]));
+
+			$key = $this->tokens->key();
 		}
 	}
 
-	protected static function nextTokenIs($tokenName, \arrayIterator $tokens, array $skipedTags = array(T_WHITESPACE, T_COMMENT, T_INLINE_HTML))
+
+	private function appendArray()
 	{
-		$key = $tokens->key() + 1;
+		$this->appendCommentAndWhitespace();
 
-		while (isset($tokens[$key]) === true && in_array($tokens[$key], $skipedTags) === true)
+		$this->tokens->next();
+
+		if ($this->tokens->valid() === true)
 		{
-			$key++;
-		}
-
-		$key++;
-
-		return (isset($tokens[$key]) === true && $tokens[$key][0] === $tokenName);
-	}
-
-	protected static function appendArray(tokenizer\iterator $iterator, \arrayIterator $tokens)
-	{
-		self::appendCommentAndWhitespace($iterator, $tokens);
-
-		$tokens->next();
-
-		if ($tokens->valid() === true)
-		{
-			$token = $tokens->current();
+			$token = $this->tokens->current();
 
 			if ($token[0] === '(')
 			{
-				$iterator->append(new tokenizer\token($token[0], isset($token[1]) === false ? null : $token[1], isset($token[2]) === false ? null : $token[2]));
+				$this->currentIterator->append(new tokenizer\token($token[0], isset($token[1]) === false ? null : $token[1], isset($token[2]) === false ? null : $token[2]));
 
 				$stack = 1;
 
-				while ($stack > 0 && $tokens->valid() === true)
+				while ($stack > 0 && $this->tokens->valid() === true)
 				{
-					$tokens->next();
+					$this->tokens->next();
 
-					if ($tokens->valid() === true)
+					if ($this->tokens->valid() === true)
 					{
-						$token = $tokens->current();
+						$token = $this->tokens->current();
 
 						if ($token[0] === '(')
 						{
@@ -306,11 +194,25 @@ class tokenizer implements \iteratorAggregate
 							$stack--;
 						}
 
-						$iterator->append(new tokenizer\token($token[0], isset($token[1]) === false ? null : $token[1], isset($token[2]) === false ? null : $token[2]));
+						$this->currentIterator->append(new tokenizer\token($token[0], isset($token[1]) === false ? null : $token[1], isset($token[2]) === false ? null : $token[2]));
 					}
 				}
 			}
 		}
+	}
+
+	private function nextTokenIs($tokenName, array $skipedTags = array(T_WHITESPACE, T_COMMENT, T_INLINE_HTML))
+	{
+		$key = $this->tokens->key() + 1;
+
+		while (isset($this->tokens[$key]) === true && in_array($this->tokens[$key], $skipedTags) === true)
+		{
+			$key++;
+		}
+
+		$key++;
+
+		return (isset($this->tokens[$key]) === true && $this->tokens[$key][0] === $tokenName);
 	}
 }
 
