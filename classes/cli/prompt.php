af60920f989589d23e85bc54a522e55200f028c1
Frédéric Hardy <frederic.hardy@mageekbox.net>
2011-07-01 14:04:01 +0200

    Add CLI prompt and using it in fields.

---
+++ classes/cli/prompt.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace mageekguy\atoum\cli;
+
+class prompt
+{
+	protected $value = '';
+	protected $colorizer = null;
+
+	public function __construct($value = '', colorizer $colorizer = null)
+	{
+		if ($colorizer === null)
+		{
+			$colorizer = new colorizer();
+		}
+
+		$this
+			->setValue($value)
+			->setColorizer($colorizer)
+		;
+	}
+
+	public function __toString()
+	{
+		return $this->colorizer->colorize($this->value);
+	}
+
+	public function setValue($value)
+	{
+		$this->value = (string) $value;
+
+		return $this;
+	}
+
+	public function getValue()
+	{
+		return $this->value;
+	}
+
+	public function setColorizer(colorizer $colorizer)
+	{
+		$this->colorizer = $colorizer;
+
+		return $this;
+	}
+
+	public function getColorizer()
+	{
+		return $this->colorizer;
+	}
+}
+
+?>
---
+++ tests/units/classes/cli/prompt.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace mageekguy\atoum\tests\units\cli;
+
+use
+	\mageekguy\atoum,
+	\mageekguy\atoum\cli
+;
+
+require_once(__DIR__ . '/../../runner.php');
+
+class prompt extends atoum\test
+{
+	public function test__construct()
+	{
+		$prompt = new cli\prompt();
+
+		$this->assert
+			->string($prompt->getValue())->isEmpty()
+			->object($prompt->getColorizer())->isInstanceOf('\mageekguy\atoum\cli\colorizer')
+			->variable($prompt->getColorizer()->getForeground())->isNull()
+			->variable($prompt->getColorizer()->getBackground())->isNull()
+		;
+
+		$prompt = new cli\prompt($value = uniqid());
+
+		$this->assert
+			->string($prompt->getValue())->isEqualTo($value)
+			->object($prompt->getColorizer())->isInstanceOf('\mageekguy\atoum\cli\colorizer')
+			->variable($prompt->getColorizer()->getForeground())->isNull()
+			->variable($prompt->getColorizer()->getBackground())->isNull()
+		;
+
+		$prompt = new cli\prompt($value = uniqid(), $colorizer = new cli\colorizer());
+
+		$this->assert
+			->string($prompt->getValue())->isEqualTo($value)
+			->object($prompt->getColorizer())->isIdenticalTo($colorizer)
+		;
+	}
+
+	public function testSetValue()
+	{
+		$prompt = new cli\prompt();
+
+		$this->assert
+			->object($prompt->setValue($value = uniqid()))->isIdenticalTo($prompt)
+			->string($prompt->getValue())->isEqualTo($value)
+			->object($prompt->setValue($value = rand(1, PHP_INT_MAX)))->isIdenticalTo($prompt)
+			->string($prompt->getValue())->isEqualTo($value)
+		;
+	}
+
+	public function testSetColorizer()
+	{
+		$prompt = new cli\prompt();
+
+		$this->assert
+			->object($prompt->setColorizer($colorizer = new cli\colorizer()))->isIdenticalTo($prompt)
+			->object($prompt->getColorizer())->isIdenticalTo($colorizer)
+		;
+	}
+
+	public function __toString()
+	{
+		$prompt = new cli\prompt();
+
+		$colorizer = new cli\colorizer(uniqid(), uniqid());
+
+		$this->assert
+			->castToString($prompt)->isEmpty()
+			->castToString($prompt->setValue($value = uniqid())->isEqualTo($value))
+			->castToString($prompt->setColorizer($colorizer)->isEqualTo($colorizer->colorize($value)))
+		;
+	}
+}
+
+?>
