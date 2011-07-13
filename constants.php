f4d489d2e5c36b21705c98ec544258fb61f9e40f
mageekguy <frederic.hardy@mageekbox.net>
2011-07-13 18:35:05 +0200

    Update repository URL.

--- constants.php
+++ constants.php
@@ -6,6 +6,6 @@ define(__NAMESPACE__ . '\directory', ($pharPath = \phar::running(false)) ? 'phar
 define(__NAMESPACE__ . '\version', preg_replace('/\$Rev: (\d+) \$/', '$1', '$Rev: 325 $'));
 define(__NAMESPACE__ . '\author', 'Frédéric Hardy');
 define(__NAMESPACE__ . '\mail', 'support@atoum.org');
-define(__NAMESPACE__ . '\repository',  'https://svn.mageekbox.net/repositories/unit/trunk');
+define(__NAMESPACE__ . '\repository',  'https://github.com/mageekguy/atoum');
 
 ?>
