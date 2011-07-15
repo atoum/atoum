646e616fded24b1abe964fe8320f81a9597977f7
Frédéric Hardy <frederic.hardy@mageekbox.net>
2011-07-15 17:06:33 +0200

    Add afterDestructOf asserter to handle object destruction and test after it mock and adapter.

---
+++ classes/asserters/afterDestructOf.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace mageekguy\atoum\asserters;
+
+use
+	\mageekguy\atoum,
+	\mageekguy\atoum\exceptions
+;
+
+class afterDestructOf extends atoum\asserter
+{
+	public function setWith($value)
+	{
+		if (is_object($value) === false)
+		{
+			$this->fail(sprintf($this->getLocale()->_('%s is not an object'), $this->getTypeOf($value)));
+		}
+		else if (method_exists($value, '__destruct') === false)
+		{
+			$this->fail(sprintf($this->getLocale()->_('Destructor of class %s is undefined'), get_class($value)));
+		}
+		else
+		{
+			$value->__destruct();
+
+			$this->pass();
+		}
+
+		return $this;
+	}
+}
+
+?>
---
+++ tests/units/classes/asserters/afterDestructOf.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace mageekguy\atoum\tests\units\asserters;
+
+use
+	\mageekguy\atoum,
+	\mageekguy\atoum\mock,
+	\mageekguy\atoum\asserter,
+	\mageekguy\atoum\asserters
+;
+
+require_once(__DIR__ . '/../../runner.php');
+
+class classWithDestructor
+{
+	public function __destruct() {}
+}
+
+class classWithoutDestructor {}
+
+class afterDestructOf extends atoum\test
+{
+	public function testClass()
+	{
+		$this->assert
+			->testedClass->isSubclassOf('\mageekguy\atoum\asserter')
+		;
+	}
+
+	public function test__construct()
+	{
+		$asserter = new asserters\afterDestructOf($generator = new asserter\generator($this));
+
+		$this->assert
+			->object($asserter->getScore())->isIdenticalTo($this->getScore())
+			->object($asserter->getLocale())->isIdenticalTo($this->getLocale())
+			->object($asserter->getGenerator())->isIdenticalTo($generator)
+		;
+	}
+
+	public function testSetWith()
+	{
+		$asserter = new asserters\afterDestructOf(new asserter\generator($test = new self($score = new atoum\score())));
+
+		$value = uniqid();
+
+		$this->assert
+			->exception(function() use (& $line, $asserter, $value) { $line = __LINE__; $asserter->setWith($value); })
+				->isInstanceOf('\mageekguy\atoum\asserter\exception')
+				->hasMessage(sprintf($test->getLocale()->_('%s is not an object'), $asserter->getTypeOf($value)))
+			->integer($score->getFailNumber())->isEqualTo(1)
+			->array($score->getFailAssertions())->isEqualTo(array(
+					array(
+						'case' => null,
+						'class' => __CLASS__,
+						'method' => $test->getCurrentMethod(),
+						'file' => __FILE__,
+						'line' => $line,
+						'asserter' => get_class($asserter) . '::setWith()',
+						'fail' => sprintf($test->getLocale()->_('%s is not an object'), $asserter->getTypeOf($value))
+					)
+				)
+			)
+			->integer($score->getPassNumber())->isZero()
+		;
+
+		$this->mock
+			->generate('mageekguy\atoum\tests\units\asserters\classWithDestructor');
+		;
+
+		$this->assert
+			->object($asserter->setWith($objectWithDestructor = new mock\mageekguy\atoum\tests\units\asserters\classWithDestructor()))->isIdenticalTo($asserter)
+			->mock($objectWithDestructor)
+				->call('__destruct')
+			->integer($score->getPassNumber())->isEqualTo(1)
+		;
+
+		$objectWithoutDestructor = new classWithoutDestructor();
+
+		$this->assert
+			->exception(function() use (& $otherLine, $asserter, $objectWithoutDestructor) { $otherLine = __LINE__; $asserter->setWith($objectWithoutDestructor); })
+				->isInstanceOf('\mageekguy\atoum\asserter\exception')
+				->hasMessage(sprintf($test->getLocale()->_('Destructor of class %s is undefined'), get_class($objectWithoutDestructor)))
+			->integer($score->getFailNumber())->isEqualTo(2)
+			->array($score->getFailAssertions())->isEqualTo(array(
+					array(
+						'case' => null,
+						'class' => __CLASS__,
+						'method' => $test->getCurrentMethod(),
+						'file' => __FILE__,
+						'line' => $line,
+						'asserter' => get_class($asserter) . '::setWith()',
+						'fail' => sprintf($test->getLocale()->_('%s is not an object'), $asserter->getTypeOf($value))
+					),
+					array(
+						'case' => null,
+						'class' => __CLASS__,
+						'method' => $test->getCurrentMethod(),
+						'file' => __FILE__,
+						'line' => $otherLine,
+						'asserter' => get_class($asserter) . '::setWith()',
+						'fail' => sprintf($test->getLocale()->_('Destructor of class %s is undefined'), get_class($objectWithoutDestructor))
+					)
+				)
+			)
+			->integer($score->getPassNumber())->isEqualTo(1)
+		;
+	}
+}
+
+?>
