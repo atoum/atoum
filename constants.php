d24d8b9e019fa0c32c9c5849b419c57f78fe3b15
Frédéric Hardy <frederic.hardy@mageekbox.net>
2011-09-16 15:27:34 +0200

    Remove useless constant.

--- constants.php
+++ constants.php
@@ -10,7 +10,6 @@ if (defined(__NAMESPACE__ . '\running') === false)
 	define(__NAMESPACE__ . '\author', 'Frédéric Hardy');
 	define(__NAMESPACE__ . '\mail', 'support@atoum.org');
 	define(__NAMESPACE__ . '\repository',  'https://github.com/mageekguy/atoum');
-	define(__NAMESPACE__ . '\services\stats',  'http://stats.atoum.org/save.php');
 }
 
 ?>
