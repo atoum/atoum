9ea84bf5f20b051be2f7e747b3c288981f083980
mageekguy <frederic.hardy@mageekbox.net>
2011-10-04 11:59:02 +0200

    Stub throw a runtime exception is PHAR extension is not available.
    Add \mageekguy\atoum\phar\name constant.
    Use it in constants.php to define atoum directory path even if PHAR
    extension is not available (thanks to @agallou).

--- scripts/phar/resources/stub.php
+++ scripts/phar/resources/stub.php
@@ -2,18 +2,23 @@
 
 namespace mageekguy\atoum;
 
-const phar = 'mageekguy.atoum.phar';
-
-\phar::mapPhar(phar);
-
 use
 	mageekguy\atoum,
 	mageekguy\atoum\scripts\phar
 ;
 
+if (extension_loaded('phar') === false)
+{
+	throw new \runtimeException('Phar extension is mandatory to use this PHAR');
+}
+
+define(__NAMESPACE__ . '\phar\name', 'mageekguy.atoum.phar');
+
+\phar::mapPhar(atoum\phar\name);
+
 if (defined(__NAMESPACE__ . '\running') === false)
 {
-	require_once 'phar://' . phar . '/classes/autoloader.php';
+	require_once 'phar://' . atoum\phar\name . '/classes/autoloader.php';
 }
 
 if (defined(__NAMESPACE__ . '\autorun') === false)
