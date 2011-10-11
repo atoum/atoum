f3560804ec3d2bc87970226e0f08b385e7ef6535
Jeremy Poulain <jerem.poulain@gmail.com>
2011-10-11 11:07:38 +0200

    Changes for owner's comment request

--- classes/asserters/string.php
+++ classes/asserters/string.php
@@ -71,40 +71,6 @@ class string extends variable
 		return parent::isEqualTo($value, $failMessage !== null ? $failMessage : $this->getLocale()->_('strings are not equals'));
 	}
 
-	public function isSha1($failMessage = null)
-	{
-		return $this->isHash(40, $failMessage);
-	}
-
-	public function isSha256($failMessage = null)
-	{
-		return $this->isHash(64, $failMessage);
-	}
-
-	public function isSha512($failMessage = null)
-	{
-		return $this->isHash(128, $failMessage);
-	}
-
-	public function isMd5($failMessage = null)
-	{
-		return $this->isHash(32, $failMessage);
-	}
-
-	protected function isHash($length, $failMessage = null)
-	{
-		if (strlen($this->valueIsSet()->value) === $length)
-		{
-			 $this->match('/^[a-f0-9]+$/', sprintf($this->getLocale()->_('%s does not match given pattern'), $this));
-		}
-		else
-		{
-			$this->fail($failMessage !== null ? $failMessage : sprintf($this->getLocale()->_('%s should be a string of %d characters'), $this, $length));
-		}
-
-		return $this;
-	}
-
 	protected static function check($value, $method)
 	{
 		if (self::isString($value) === false)
