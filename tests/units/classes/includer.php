9f78fd33a97adbdf66cbecac26d5be61ef721afb
mageekguy <frederic.hardy@mageekbox.net>
2011-12-05 12:05:01 +0100

    Typo.

--- tests/units/classes/includer.php
+++ tests/units/classes/includer.php
@@ -22,10 +22,12 @@ class includer extends atoum\test
 					->hasMessage('Unable to include \'atoum://file/not/exists\'')
 			->if($fileExistsController = mock\stream::get('file/exists'))
 			->and($fileExistsController->file_get_contents = $fileContents = uniqid())
+			->then
 				->object($includer->includePath('atoum://file/exists'))->isIdenticalTo($includer)
 				->output->isEqualTo($fileContents)
 			->if($fileWithAnErrorController = mock\stream::get('file/with/an/error'))
 			->and($fileWithAnErrorController->file_get_contents = '<?php trigger_error(\'' . ($message = uniqid()) . '\', E_USER_WARNING); ?>')
+			->then
 				->object($includer->includePath('atoum://file/with/an/error'))->isIdenticalTo($includer)
 				->error
 					->withType(E_USER_WARNING)
