0b4740c87bdf8568a530c36cb5723276acb5b23c
mageekguy <frederic.hardy@mageekbox.net>
2011-12-30 13:44:33 +0100

    Improve filter src iterator.

--- classes/src/iterator/filter.php
+++ classes/src/iterator/filter.php
@@ -8,17 +8,14 @@ use
 
 class filter extends \recursiveFilterIterator
 {
-	function accept()
+	function accept(\splFileInfo $file = null)
 	{
-		$file = $this->getInnerIterator()->current();
+		if ($file === null)
+		{
+			$file = $this->getInnerIterator()->current();
+		}
 
-		return (
-				substr($file->getFilename(), 0, 1) != '.'
-				&&
-				basename(dirname($file->getPathname())) != 'tmp'
-				&&
-				preg_match('#/configurations/.+\.php$#', $file->getPathname()) === 0
-		);
+		return (substr($file->getFilename(), 0, 1) != '.');
 	}
 }
 
