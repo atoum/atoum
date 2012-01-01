ebc69badbc91f6dd6352965ae232b724641f1284
mageekguy <frederic.hardy@mageekbox.net>
2012-01-01 15:41:32 +0100

    Windows compatibility.

--- tests/units/classes/autoloader.php
+++ tests/units/classes/autoloader.php
@@ -7,7 +7,6 @@ use
 ;
 
 require_once __DIR__ . '/../runner.php';
-require_once __DIR__ . '/../../../constants.php';
 
 class autoloader extends atoum\test
 {
@@ -17,9 +16,9 @@ class autoloader extends atoum\test
 
 		$this->assert
             ->array($directories = $autoloader->getDirectories())->hasKey('mageekguy\atoum')
-            ->array($directories['mageekguy\atoum'])->isEqualTo(array(atoum\directory . '/classes'));
+            ->array($directories['mageekguy\atoum'])->isEqualTo(array(atoum\directory . DIRECTORY_SEPARATOR . 'classes'));
 	}
-    
+
 	public function testGetPath()
 	{
 		$autoloader = new atoum\autoloader();
@@ -28,7 +27,7 @@ class autoloader extends atoum\test
 			->variable($autoloader->getPath(uniqid()))->isNull()
 			->variable($autoloader->getPath('mageekguy\atoum'))->isNull()
 			->variable($autoloader->getPath('\mageekguy\atoum'))->isNull()
-			->string($autoloader->getPath('mageekguy\atoum\test'))->isEqualTo(atoum\directory . '/classes/test.php')
+			->string($autoloader->getPath('mageekguy\atoum\test'))->isEqualTo(atoum\directory . DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR . 'test.php')
 			->variable($autoloader->getPath('\mageekguy\atoum\test'))->isNull()
 		;
 	}
