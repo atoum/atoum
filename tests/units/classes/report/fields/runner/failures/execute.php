88aa173af62536a535b331595b11da39c046cec3
mageekguy <frederic.hardy@mageekbox.net>
2012-09-12 20:42:42 +0200

    Improve unit tests about execute field.

--- tests/units/classes/report/fields/runner/failures/execute.php
+++ tests/units/classes/report/fields/runner/failures/execute.php
@@ -11,6 +11,13 @@ require_once __DIR__ . '/../../../../../runner.php';
 
 class execute extends atoum\test
 {
+	public function testClass()
+	{
+		$this
+			->testedClass->isSubclassOf('mageekguy\atoum\report\fields\runner\failures')
+		;
+	}
+
 	public function test__construct()
 	{
 		$this
@@ -30,10 +37,51 @@ class execute extends atoum\test
 	public function test__toString()
 	{
 		$this
-			->if($field = new testedClass($command = uniqid(), $adapter = new atoum\test\adapter()))
+			->if($field = new testedClass($command = '%1$s %2$s', $adapter = new atoum\test\adapter()))
+			->and($adapter->system = function() {})
 			->then
 				->castToString($field)->isEmpty()
 				->adapter($adapter)->call('system')->never()
+			->if($score = new \mock\mageekguy\atoum\runner\score())
+			->and($score->getMockController()->getErrors = array())
+			->and($runner = new atoum\runner())
+			->and($runner->setScore($score))
+			->and($field->handleEvent(atoum\runner::runStart, $runner))
+			->then
+				->castToString($field)->isEmpty()
+				->adapter($adapter)->call('system')->never()
+			->if($field->handleEvent(atoum\runner::runStop, $runner))
+			->then
+				->castToString($field)->isEmpty()
+				->adapter($adapter)->call('system')->never()
+			->if($score->getMockController()->getFailAssertions = $fails = array(
+						array(
+							'case' => null,
+							'dataSetKey' => null,
+							'class' => $class = uniqid(),
+							'method' => $method = uniqid(),
+							'file' => $file = uniqid(),
+							'line' => $line = uniqid(),
+							'asserter' => $asserter = uniqid(),
+							'fail' => $fail = uniqid()
+						),
+						array(
+							'case' => null,
+							'dataSetKey' => null,
+							'class' => $otherClass = uniqid(),
+							'method' => $otherMethod = uniqid(),
+							'file' => $otherFile = uniqid(),
+							'line' => $otherLine = uniqid(),
+							'asserter' => $otherAsserter = uniqid(),
+							'fail' => $otherFail = uniqid()
+						)
+					)
+				)
+			->if($field->handleEvent(atoum\runner::runStop, $runner))
+			->then
+				->castToString($field)->isEmpty()
+				->adapter($adapter)->call('system')->withArguments(sprintf($command, $file, $line))->once()
+				->adapter($adapter)->call('system')->withArguments(sprintf($command, $otherFile, $otherLine))->once()
 		;
 	}
 
@@ -54,6 +102,18 @@ class execute extends atoum\test
 			->then
 				->object($field->setAdapter($adapter = new atoum\adapter()))->isIdenticalTo($field)
 				->object($field->getAdapter())->isEqualTo($adapter)
+		;
+	}
+
+	public function testHandleEvent()
+	{
+		$this
+			->if($field = new testedClass(uniqid()))
+			->then
+				->boolean($field->handleEvent(atoum\runner::runStart, new atoum\runner()))->isFalse()
+				->variable($field->getRunner())->isNull()
+				->boolean($field->handleEvent(atoum\runner::runStop, $runner = new atoum\runner()))->isTrue()
+				->object($field->getRunner())->isIdenticalTo($runner)
 		;
 	}
 }
