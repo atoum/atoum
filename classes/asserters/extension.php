1f76068e380237c58c55e7529f7f7b2141ec5584
mageekguy <frederic.hardy@mageekbox.net>
2012-10-29 13:38:19 +0100

    Add extension asserter to allow skipping of test which need specific PHP extension.

---
+++ classes/asserters/extension.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace mageekguy\atoum\asserters;
+
+use
+	mageekguy\atoum,
+	mageekguy\atoum\test,
+	mageekguy\atoum\exceptions
+;
+
+class extension extends atoum\asserter
+{
+	protected $name = null;
+	protected $adapter = null;
+
+	public function __construct(atoum\asserter\generator $generator)
+	{
+		parent::__construct($generator);
+
+		$this->setAdapter();
+	}
+
+	public function __toString()
+	{
+		return (string) $this->name;
+	}
+
+	public function setWith($name)
+	{
+		$this->name = $name;
+
+		return $this;
+	}
+
+	public function setAdapter(atoum\adapter $adapter = null)
+	{
+		$this->adapter = $adapter ?: new atoum\adapter();
+
+		return $this;
+	}
+
+	public function getAdapter()
+	{
+		return $this->adapter;
+	}
+
+	public function reset()
+	{
+		$this->name = null;
+
+		return $this;
+	}
+
+	public function getName()
+	{
+		return $this->name;
+	}
+
+	public function isLoaded($failMessage = null)
+	{
+		if ($this->valueIsSet()->adapter->extension_loaded($this->name) === true)
+		{
+			return $this->pass();
+		}
+		else
+		{
+			$this->fail($failMessage !== null ? $failMessage : sprintf($this->getLocale()->_('PHP extension \'%s\' is not loaded'), $this));
+		}
+	}
+
+	protected function valueIsSet($message = 'Name of PHP extension is undefined')
+	{
+		if ($this->name === null)
+		{
+			throw new exceptions\logic($message);
+		}
+
+		return $this;
+	}
+
+	protected function pass()
+	{
+		return $this;
+	}
+
+	protected function fail($reason)
+	{
+		throw new test\exceptions\skip($reason);
+	}
+}
---
+++ tests/units/classes/asserters/extension.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace mageekguy\atoum\tests\units\asserters;
+
+use
+	mageekguy\atoum,
+	mageekguy\atoum\asserter,
+	mageekguy\atoum\asserters\extension as testedClass
+;
+
+require_once __DIR__ . '/../../runner.php';
+
+class extension extends atoum\test
+{
+	public function testClass()
+	{
+		$this->testedClass->extends('mageekguy\atoum\asserter');
+	}
+
+	public function test__construct()
+	{
+		$this
+			->if($asserter = new testedClass($generator = new asserter\generator()))
+			->then
+				->object($asserter->getLocale())->isIdenticalTo($generator->getLocale())
+				->object($asserter->getGenerator())->isIdenticalTo($generator)
+				->object($asserter->getAdapter())->isEqualTo(new atoum\adapter())
+				->variable($asserter->getName())->isNull()
+		;
+	}
+
+	public function test__toString()
+	{
+		$this
+			->if($asserter = new testedClass(new asserter\generator()))
+			->then
+				->castToString($asserter)->isEmpty()
+			->if($asserter->setWith($extensionName = uniqid()))
+			->then
+				->castToString($asserter)->isEqualTo($extensionName)
+		;
+	}
+
+	public function testSetWith()
+	{
+		$this
+			->if($asserter = new testedClass(new asserter\generator()))
+			->then
+				->object($asserter->setWith($extensionName = uniqid()))->isIdenticalTo($asserter)
+				->string($asserter->getName())->isEqualTo($extensionName)
+		;
+	}
+
+	public function testSetAdapter()
+	{
+		$this
+			->if($asserter = new testedClass(new asserter\generator()))
+			->then
+				->object($asserter->setAdapter($adapter = new atoum\adapter()))->isIdenticalTo($asserter)
+				->object($asserter->getAdapter())->isIdenticalTo($adapter)
+				->object($asserter->setAdapter())->isIdenticalTo($asserter)
+				->object($asserter->getAdapter())
+					->isNotIdenticalTo($adapter)
+					->isEqualTo(new atoum\adapter())
+		;
+	}
+
+	public function testReset()
+	{
+		$this
+			->if($asserter = new testedClass(new asserter\generator()))
+			->then
+				->object($asserter->reset())->isIdenticalTo($asserter)
+				->variable($asserter->getName())->isNull()
+			->if($asserter->setWith(uniqid()))
+			->then
+				->object($asserter->reset())->isIdenticalTo($asserter)
+				->variable($asserter->getName())->isNull()
+		;
+	}
+
+	public function testIsLoaded()
+	{
+		$this
+			->if($asserter = new testedClass(new asserter\generator()))
+			->then
+				->exception(function() use ($asserter) {
+						$asserter->isLoaded();
+					}
+				)
+					->isInstanceOf('mageekguy\atoum\exceptions\logic')
+					->hasMessage('Name of PHP extension is undefined')
+			->if($asserter->setAdapter($adapter = new atoum\test\adapter()))
+			->and($adapter->extension_loaded = false)
+			->and($asserter->setWith($extensionName = uniqid()))
+			->then
+				->exception(function() use ($asserter) {
+						$asserter->isLoaded();
+					}
+				)
+					->isInstanceOf('mageekguy\atoum\test\exceptions\skip')
+					->hasMessage('PHP extension \'' . $extensionName . '\' is not loaded')
+			->if($adapter->extension_loaded = true)
+			->then
+				->object($asserter->isLoaded())->isIdenticalTo($asserter)
+		;
+	}
+}
