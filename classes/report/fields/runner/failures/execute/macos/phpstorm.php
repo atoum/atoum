2ce759d6763d5f8f4928e216c451891f4639b39e
jubianchi <contact@jubianchi.fr>
2012-11-06 23:24:05 +0100

    Fix Mac OS PhpStorm field

--- classes/report/fields/runner/failures/execute/macos/phpstorm.php
+++ classes/report/fields/runner/failures/execute/macos/phpstorm.php
@@ -2,9 +2,10 @@
 
 namespace mageekguy\atoum\report\fields\runner\failures\execute\macos;
 
-
 use
 	mageekguy\atoum,
+	mageekguy\atoum\locale,
+	mageekguy\atoum\adapter,
 	mageekguy\atoum\report\fields\runner\failures
 ;
 
@@ -13,6 +14,12 @@ class phpstorm extends failures\execute
 	public function __construct($command = null)
 	{
 		$command = $command ?: '/Applications/PhpStorm.app/Contents/MacOS/webide';
-		$this->command = $command . ' --line %2$d %1$s &> /dev/null &';
+
+		parent::__construct($command);
+	}
+
+	public function setCommand($command)
+	{
+		return parent::setCommand($command . ' --line %2$d %1$s &> /dev/null &');
 	}
 }
