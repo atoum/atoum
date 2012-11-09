f3a9b8c3edb9c1061bc8e9f4698e02e74ad32b64
mageekguy <frederic.hardy@mageekbox.net>
2012-11-09 14:03:14 +0100

    Add plan field for TAP report.

---
+++ classes/report/fields/runner/tap/plan.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace mageekguy\atoum\report\fields\runner\tap;
+
+use
+	mageekguy\atoum\runner,
+	mageekguy\atoum\report
+;
+
+class plan extends report\field
+{
+	protected $testMethodNumber = 0;
+
+	public function __construct()
+	{
+		parent::__construct(array(runner::runStart));
+	}
+
+	public function __toString()
+	{
+		return ($this->testMethodNumber <= 0 ? '' : '1..' . $this->testMethodNumber);
+	}
+
+	public function handleEvent($event, \mageekguy\atoum\observable $observable)
+	{
+		if (parent::handleEvent($event, $observable) === false)
+		{
+			return false;
+		}
+		else
+		{
+			$this->testMethodNumber = $observable->getTestMethodNumber();
+
+			return true;
+		}
+	}
+}
---
+++ classes/reports/realtime/tap.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace mageekguy\atoum\reports\realtime;
+
+use
+	mageekguy\atoum\reports
+;
+
+class tap extends reports\realtime
+{
+}
