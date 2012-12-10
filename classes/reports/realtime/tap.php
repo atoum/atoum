315929c137b9710ad1126f8edb3e14a8cc43b7fb
mageekguy <frederic.hardy@mageekbox.net>
2012-12-10 14:11:25 +0100

    Improve TAP support.

--- classes/reports/realtime/tap.php
+++ classes/reports/realtime/tap.php
@@ -3,9 +3,19 @@
 namespace mageekguy\atoum\reports\realtime;
 
 use
-	mageekguy\atoum\reports
+	mageekguy\atoum\reports,
+	mageekguy\atoum\report\fields
 ;
 
 class tap extends reports\realtime
 {
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this
+			->addField(new fields\runner\tap\plan())
+			->addField(new fields\test\event\tap())
+		;
+	}
 }
