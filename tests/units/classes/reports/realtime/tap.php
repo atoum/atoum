93d743dd1cbdedaeacca41d87e65b2802197eabc
mageekguy <frederic.hardy@mageekbox.net>
2012-12-10 14:11:25 +0100

    Improve TAP support.

--- tests/units/classes/reports/realtime/tap.php
+++ tests/units/classes/reports/realtime/tap.php
@@ -3,7 +3,9 @@
 namespace mageekguy\atoum\tests\units\reports\realtime;
 
 use
-	mageekguy\atoum
+	mageekguy\atoum,
+	mageekguy\atoum\report\fields,
+	mageekguy\atoum\reports\realtime\tap as testedClass
 ;
 
 require __DIR__ . '/../../../runner.php';
@@ -14,6 +16,23 @@ class tap extends atoum\test
 	{
 		$this->testedClass
 			->extends('mageekguy\atoum\reports\realtime')
+		;
+	}
+
+	public function test__construct()
+	{
+		$this
+			->define($planField = new fields\runner\tap\plan())
+			->define($eventField = new fields\test\event\tap())
+			->if($report = new testedClass())
+			->then
+				->object($report->getLocale())->isEqualTo(new atoum\locale())
+				->object($report->getAdapter())->isEqualTo(new atoum\adapter())
+				->array($report->getFields())->isEqualTo(array(
+						$planField,
+						$eventField
+					)
+				)
 		;
 	}
 }
