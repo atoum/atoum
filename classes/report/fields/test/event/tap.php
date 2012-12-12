b8be6b2aaf8b99c61887eb708d4813f789d5e3e5
mageekguy <frederic.hardy@mageekbox.net>
2012-12-12 09:41:08 +0100

    Add description in test line.

--- classes/report/fields/test/event/tap.php
+++ classes/report/fields/test/event/tap.php
@@ -3,6 +3,7 @@
 namespace mageekguy\atoum\report\fields\test\event;
 
 use
+	mageekguy\atoum,
 	mageekguy\atoum\test,
 	mageekguy\atoum\runner,
 	mageekguy\atoum\report,
@@ -12,6 +13,7 @@ use
 class tap extends report\fields\event
 {
 	protected $testPointNumber = 0;
+	protected $description = '';
 
 	public function __construct()
 	{
@@ -29,6 +31,34 @@ class tap extends report\fields\event
 		);
 	}
 
+	public function handleEvent($event, atoum\observable $observable)
+	{
+		$eventHandled = parent::handleEvent($event, $observable);
+
+		if ($eventHandled === true)
+		{
+			switch ($this->event)
+			{
+				case runner::runStart:
+					$this->testPointNumber = 0;
+					break;
+
+				case test::success:
+					$this->testPointNumber++;
+					$this->description = '';
+					break;
+
+				case test::fail:
+					$lastFailures = current(array_slice($observable->getScore()->getFailAssertions(), -1));
+					$this->testPointNumber++;
+					$this->description = $lastFailures['fail'];
+					break;
+			}
+		}
+
+		return $eventHandled;
+	}
+
 	public function __toString()
 	{
 		$string = '';
@@ -42,7 +72,7 @@ class tap extends report\fields\event
 					break;
 
 				case test::success:
-					$string = 'ok ' . (++$this->testPointNumber) . PHP_EOL;
+					$string = 'ok ' . $this->testPointNumber . PHP_EOL;
 					break;
 
 				case test::fail:
@@ -51,7 +81,7 @@ class tap extends report\fields\event
 				case test::void:
 				case test::uncompleted:
 				case test::skipped:
-					$string = 'not ok ' . (++$this->testPointNumber) . PHP_EOL;
+					$string = 'not ok ' . $this->testPointNumber . ' - ' . $this->description . PHP_EOL;
 					break;
 			}
 		}
