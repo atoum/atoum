e6e9382a3c5790b17ec31e7b9c1aa142b061d8b5
mageekguy <frederic.hardy@mageekbox.net>
2013-03-05 09:39:03 +0100

    Remove useless directory in autoloader.

--- tests/units/runner.php
+++ tests/units/runner.php
@@ -2,9 +2,10 @@
 
 namespace mageekguy\atoum\tests\units;
 
+use
+	mageekguy\atoum
+;
+
 require_once __DIR__ . '/../../scripts/runner.php';
 
-\mageekguy\atoum\autoloader::get()
-	->addDirectory(__NAMESPACE__, __DIR__ . '/classes')
-	->addDirectory(__NAMESPACE__ . '\asserters', __DIR__ . '/asserters')
-;
+atoum\autoloader::get()->addDirectory(__NAMESPACE__ . '\asserters', __DIR__ . '/asserters');
