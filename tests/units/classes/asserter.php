8ed32e9cf7ec9ae952ccf2ca0a048d934df46753
mageekguy <frederic.hardy@mageekbox.net>
2013-04-03 13:03:06 +0200

    Improve unit tests about asserter, in order to secure merge of PR #190.

--- tests/units/classes/asserter.php
+++ tests/units/classes/asserter.php
@@ -6,6 +6,7 @@ require __DIR__ . '/../runner.php';
 
 use
 	mageekguy\atoum,
+	mageekguy\atoum\asserters,
 	mock\mageekguy\atoum\asserter as testedClass
 ;
 
@@ -17,9 +18,63 @@ class asserter extends atoum\test
 			->if($asserter = new testedClass($generator = new atoum\asserter\generator()))
 			->then
 				->object($asserter->getGenerator())->isIdenticalTo($generator)
+				->object($asserter->getLocale())->isIdenticalTo($generator->getLocale())
 			->if($asserter = new testedClass())
 			->then
-				->object($asserter->getGenerator())->isEqualTo(new atoum\asserter\generator())
+				->object($generator = $asserter->getGenerator())->isEqualTo(new atoum\asserter\generator())
+				->object($asserter->getLocale())->isIdenticalTo($generator->getLocale())
+		;
+	}
+
+	public function test__get()
+	{
+		$this
+			->if($asserter = new testedClass($generator = new atoum\asserter\generator()))
+			->then
+				->object($integerAsserter = $asserter->integer)->isEqualTo(new asserters\integer($generator))
+				->object($integerAsserter->getGenerator())->isIdenticalTo($generator)
+		;
+	}
+
+	public function test__call()
+	{
+		$this
+			->if($asserter = new testedClass($generator = new atoum\asserter\generator()))
+			->then
+				->object($integerAsserter = $asserter->integer($integer = rand(1, PHP_INT_MAX)))->isInstanceOf('mageekguy\atoum\asserters\integer')
+				->integer($integerAsserter->getValue())->isEqualTo($integer)
+				->object($integerAsserter->getGenerator())->isIdenticalTo($generator)
+		;
+	}
+
+	public function testReset()
+	{
+		$this
+			->if($asserter = new testedClass(new atoum\asserter\generator()))
+			->then
+				->object($asserter->reset())->isIdenticalTo($asserter)
+		;
+	}
+
+	public function testSetLocale()
+	{
+		$this
+			->if($asserter = new testedClass(new atoum\asserter\generator()))
+			->then
+				->object($asserter->setLocale($locale = new atoum\locale()))->isIdenticalTo($asserter)
+				->object($asserter->getLocale())->isIdenticalTo($locale)
+		;
+	}
+
+	public function testGetLocale()
+	{
+		$this
+			->if($asserter = new testedClass($generator = new atoum\asserter\generator()))
+			->then
+				->object($asserter->getLocale())->isIdenticalTo($generator->getLocale())
+			->if($asserter->setLocale($locale = new atoum\locale()))
+			->then
+				->object($asserter->getLocale())->isIdenticalTo($locale)
 		;
 	}
 
@@ -30,6 +85,7 @@ class asserter extends atoum\test
 			->then
 				->object($asserter->setGenerator($generator = new atoum\asserter\generator()))->isIdenticalTo($asserter)
 				->object($asserter->getGenerator())->isIdenticalTo($generator)
+				->object($asserter->getLocale())->isIdenticalTo($generator->getLocale())
 				->object($asserter->setGenerator())->isIdenticalTo($asserter)
 				->object($asserter->getGenerator())
 					->isNotIdenticalTo($generator)
@@ -55,6 +111,33 @@ class asserter extends atoum\test
 				->mock($asserter)->call('setWith')->never()
 				->object($asserter->setWithArguments(array($argument = uniqid())))->isIdenticalTo($asserter)
 				->mock($asserter)->call('setWith')->withArguments($argument)->once()
+		;
+	}
+
+	public function testGetTypeOf()
+	{
+		$this
+			->if($asserter = new testedClass(new atoum\asserter\generator()))
+			->and($asserter->setLocale($locale = new \mock\mageekguy\atoum\locale()))
+			->then
+				->string($asserter->getTypeOf(true))->isEqualTo('boolean(true)')
+				->mock($locale)->call('_')->withArguments('boolean(%s)')->once()
+				->string($asserter->getTypeOf(false))->isEqualTo('boolean(false)')
+				->mock($locale)->call('_')->withArguments('boolean(%s)')->twice()
+				->string($asserter->getTypeOf($integer = rand(1, PHP_INT_MAX)))->isEqualTo('integer(' . $integer . ')')
+				->mock($locale)->call('_')->withArguments('integer(%s)')->once()
+				->string($asserter->getTypeOf($float = (float) rand(1, PHP_INT_MAX)))->isEqualTo('float(' . $float . ')')
+				->mock($locale)->call('_')->withArguments('float(%s)')->once()
+				->string($asserter->getTypeOf(null))->isEqualTo('null')
+				->mock($locale)->call('_')->withArguments('null')->once()
+				->string($asserter->getTypeOf($this))->isEqualTo('object(' . get_class($this) . ')')
+				->mock($locale)->call('_')->withArguments('object(%s)')->once()
+				->string($asserter->getTypeOf($resource = fopen(__FILE__, 'r')))->isEqualTo('resource(' . $resource . ')')
+				->mock($locale)->call('_')->withArguments('resource(%s)')->once()
+				->string($asserter->getTypeOf('string'))->isEqualTo('string(6) \'string\'')
+				->mock($locale)->call('_')->withArguments('string(%s) \'%s\'')->once()
+				->string($asserter->getTypeOf(range(1, 10)))->isEqualTo('array(10)')
+				->mock($locale)->call('_')->withArguments('array(%s)')->once()
 		;
 	}
 }
