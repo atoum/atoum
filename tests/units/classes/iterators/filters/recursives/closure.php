b0f82c9506a1fd56e31ba1a01d0b752d0c0bf05d
mageekguy <frederic.hardy@mageekbox.net>
2013-04-10 17:30:07 +0200

    Improve unit tests.

--- tests/units/classes/iterators/filters/recursives/closure.php
+++ tests/units/classes/iterators/filters/recursives/closure.php
@@ -6,34 +6,45 @@ require __DIR__ . '/../../../../runner.php';
 
 use
 	mageekguy\atoum,
-	mageekguy\atoum\mock,
-	mageekguy\atoum\iterators\filters\recursives
+	mageekguy\atoum\iterators\filters\recursives\closure as testedClass
 ;
 
 class closure extends atoum\test
 {
-	public function testDeepFilter()
+	public function testAddClosure()
 	{
-		$array = array(
-			0,
-			1,
-			2,
-			array(
-				3,
-				array(
-					4,
-					5
-				),
-				6
-			),
-			7,
-			8,
-			9
-		);
-
 		$this
-			->if($iterator = new \recursiveIteratorIterator(
-					new recursives\closure(
+			->if($filter = new testedClass(new \recursiveArrayIterator(array())))
+			->then
+				->object($filter->addClosure($closure = function() {}))->isIdenticalTo($filter)
+				->array($filter->getClosures())->isEqualTo(array($closure))
+				->object($filter->addClosure($otherClosure = function() {}))->isIdenticalTo($filter)
+				->array($filter->getClosures())->isEqualTo(array($closure, $otherClosure))
+		;
+	}
+
+	public function testAccept()
+	{
+		$this
+			->if($array = array(
+					0,
+					1,
+					2,
+					array(
+						3,
+						array(
+							4,
+							5
+						),
+						6
+					),
+					7,
+					8,
+					9
+				)
+			)
+			->and($iterator = new \recursiveIteratorIterator(
+					new testedClass(
 						new \recursiveArrayIterator($array),
 						function($current, $key, \recursiveArrayIterator $innerIterator)
 						{
@@ -49,8 +60,8 @@ class closure extends atoum\test
 			)
 			->then
 				->array(iterator_to_array($iterator, false))
-				->hasSize(5)
-				->strictlyContainsValues(array(0, 2, 4, 6, 8))
-				->strictlyNotContainsValues(array(1, 3, 5, 7, 9));
+					->hasSize(5)
+					->strictlyContainsValues(array(0, 2, 4, 6, 8))
+					->strictlyNotContainsValues(array(1, 3, 5, 7, 9));
 	}
 }
