aaef0d40ae59cddbc883f719531155d1bcb003e8
mageekguy <frederic.hardy@mageekbox.net>
2013-05-13 10:13:51 +0200

    Improve unit test.

--- tests/units/classes/adapter.php
+++ tests/units/classes/adapter.php
@@ -3,7 +3,8 @@
 namespace mageekguy\atoum\tests\units;
 
 use
-	atoum
+	atoum,
+	atoum\adapter as testedClass
 ;
 
 require_once __DIR__ . '/../runner.php';
@@ -13,9 +14,18 @@ class adapter extends atoum
 	public function test__call()
 	{
 		$this
-			->given($adapter = new atoum\adapter())
+			->given($adapter = new testedClass())
 			->then
 				->string($adapter->md5($hash = uniqid()))->isEqualTo(md5($hash))
+		;
+	}
+
+	public function testInvoke()
+	{
+		$this
+			->given($adapter = new testedClass())
+			->then
+				->string($adapter->invoke('md5', array($hash = uniqid())))->isEqualTo(md5($hash))
 		;
 	}
 }
