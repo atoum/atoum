ed4be6f215a88f59e682390d8ee9911869c78cd8
mageekguy <frederic.hardy@mageekbox.net>
2013-05-13 10:12:33 +0200

    Cleaning.

--- tests/units/classes/adapter.php
+++ tests/units/classes/adapter.php
@@ -10,15 +10,6 @@ require_once __DIR__ . '/../runner.php';
 
 class adapter extends atoum
 {
-	public function test__construct()
-	{
-		$this
-			->given($asserter = new \mock\mageekguy\atoum\asserter($generator = new atoum\asserter\generator()))
-			->then
-				->object($asserter->getGenerator())->isIdenticalTo($generator)
-		;
-	}
-
 	public function test__call()
 	{
 		$this
