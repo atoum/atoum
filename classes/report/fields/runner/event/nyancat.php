069344f33b6741ef7d2f059f5d6a625403c72136
jubianchi <contact@jubianchi.fr>
2013-09-04 23:35:41 +0200

    Improve nyancat

--- classes/report/fields/runner/event/nyancat.php
+++ classes/report/fields/runner/event/nyancat.php
@@ -8,95 +8,108 @@ use
 	mageekguy\atoum\report,
 	mageekguy\atoum\exceptions,
 	mageekguy\atoum\cli\progressBar,
-    mageekguy\atoum\cli\colorizer
+	mageekguy\atoum\cli\colorizer
 ;
+use mageekguy\atoum;
 
 
 class nyancat extends cli
 {
 	protected $cat = array(
 		array(
-			'  ,-------,  ',
-			' ┓|       ^__^ ',
-			' ┗|     |｡◕‿‿◕｡| ',
-			'  ╰OO----OO  ',
+			'  ,------,  ',
+			' ┓|      ^__^ ',
+			' ┗|    |｡◕‿‿◕｡| ',
+			'  ╰Oo---Oo  ',
 		),
 		array(
-			'  ,---------,',
-			' ┓|        ^__^',
-			' ┗|      |｡◕‿‿◕｡|',
-			'  ╰O-O----O-O',
+			'  ,--------,',
+			' ┓|       ^__^',
+			' ┗|     |｡◕‿‿◕｡|',
+			'  ╰o-O---o-O',
 		),
 	);
-    protected $curve = "`·.,¸,.·'¯";
+	protected $curve = "`·.,¸,.·'¯";
 	protected $offset = 0;
-    protected $catColorizer;
-    protected $rainbowColorizers;
+	protected $catColorizer;
+	protected $rainbowColorizers;
 	protected $nyan = '';
+	protected $utime = 0;
 
-    public function __construct(progressBar $progressBar = null)
-    {
-        parent::__construct($progressBar);
-
-        $this->catColorizer = new colorizer(32);
-
-        $this->rainbowColorizers = array(
-            new colorizer(31),
-            new colorizer(32),
-            new colorizer(33),
-            new colorizer(34),
-            new colorizer(35),
-            new colorizer(36)
-        );
-    }
-
-    public function __toString()
+	public function __construct(progressBar $progressBar = null)
 	{
-        $string = PHP_EOL;
+		parent::__construct($progressBar);
 
-        if ($this->event === test::fail)
-        {
-            $this->catColorizer = new colorizer(31);
-        }
+		$this->catColorizer = new colorizer(32);
+
+		$this->rainbowColorizers = array(
+			new colorizer(31),
+			new colorizer(32),
+			new colorizer(33),
+			new colorizer(34),
+			new colorizer(35),
+			new colorizer(36)
+		);
+	}
+
+	protected function clear()
+	{
+		if (empty($this->nyan) === false)
+		{
+			$lines = explode(PHP_EOL, $this->nyan);
+			return "\x1b[" . (count($lines) - 1) . "F";
+		}
+	}
+
+	public function __toString()
+	{
+		if (in_array($this->event, array(test::fail, test::error, test::exception, test::uncompleted)))
+		{
+			$this->catColorizer = new colorizer(31);
+		}
+
+		if(microtime(true) - $this->utime < 0.15) {
+			return '';
+		}
+
+		$string = PHP_EOL;
 
 		if ($this->event !== runner::runStop)
 		{
-            $cat = $this->cat[$this->offset % 2];
+			$cat = $this->cat[$this->offset % 2];
 
-            if (empty($this->nyan) === false)
-            {
-                $lines = explode(PHP_EOL, $this->nyan);
-                $string .= "\x1b[" . (count($lines) - 1) . "F";
-            }
+			$string .= $this->clear();
 
-            for ($row = 0; $row < count($cat); $row++)
-            {
-                $string .= $this->getRainbowRow() . $this->catColorizer->colorize($cat[$row]) . PHP_EOL;
-            }
+			for ($row = 0; $row < count($cat); $row++)
+			{
+				$string .= $this->getRainbowRow() . $this->catColorizer->colorize($cat[$row]) . PHP_EOL;
+			}
 
-            $string .= PHP_EOL;
+			$string .= PHP_EOL;
 
-            $this->offset = $this->offset + 1 === count($this->rainbowColorizers)
-                ? 0
-                : $this->offset + 1;
+			$this->offset = $this->offset + 1 === count($this->rainbowColorizers)
+				? 0
+				: $this->offset + 1;
 
-            $this->nyan = $string;
-        }
+			$this->nyan = $string;
+		}
 
-        return $string;
+		$this->utime = microtime(true);
+
+		return $string;
 	}
 
-    protected function getRainbowRow()
-    {
-        $string = '';
+	protected function getRainbowRow()
+	{
+		$string = '';
 
-        for ($column = 0; $column < count($this->rainbowColorizers); $column++)
-        {
-            $string .= $this->getColorizer($column - $this->offset)->colorize($this->curve);
-        }
+		for ($column = 0; $column < count($this->rainbowColorizers); $column++)
+		{
+			$string .= $this->getColorizer($column - $this->offset)->colorize($this->curve);
+		}
 
-        return $string;
-    }
+		return $string;
+	}
 
 	protected function getColorizer($offset)
 	{
