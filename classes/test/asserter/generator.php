d7e35945e5901a883153c922ae9d0aab7fbe04c0
mageekguy <frederic.hardy@mageekbox.net>
2014-03-17 13:11:31 +0100

    Remove useless methods.

--- classes/test/asserter/generator.php
+++ classes/test/asserter/generator.php
@@ -40,37 +40,6 @@ class generator extends asserter\generator
 		return $this->test;
 	}
 
-	public function asserterPass(atoum\asserter $asserter)
-	{
-		$this->test->getScore()->addPass();
-
-		return $this;
-	}
-
-	public function asserterFail(atoum\asserter $asserter, $reason)
-	{
-		$class = $this->test->getClass();
-		$method = $this->test->getCurrentMethod();
-		$file = $this->test->getPath();
-		$line = null;
-		$function = null;
-
-		foreach (array_filter(debug_backtrace(false), function($backtrace) use ($file) { return isset($backtrace['file']) === true && $backtrace['file'] === $file; }) as $backtrace)
-		{
-			if ($line === null && isset($backtrace['line']) === true)
-			{
-				$line = $backtrace['line'];
-			}
-
-			if ($function === null && isset($backtrace['object']) === true && isset($backtrace['function']) === true && $backtrace['object'] === $asserter && $backtrace['function'] !== '__call')
-			{
-				$function = $backtrace['function'];
-			}
-		}
-
-		throw new asserter\exception($reason, $this->test->getScore()->addFail($file, $class, $method, $line, get_class($asserter) . ($function ? '::' . $function : '') . '()', $reason));
-	}
-
 	public function getAsserterInstance($asserter, array $arguments = array(), atoum\test $test = null)
 	{
 		return parent::getAsserterInstance($asserter, $arguments, $test ?: $this->test);
