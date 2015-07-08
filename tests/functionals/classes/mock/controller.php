ee4ac7c2d1ae1395b1a443853d1e4b93bb0d3821
jubianchi <contact@jubianchi.fr>
2015-07-08 15:00:28 +0200

    Add a test to check the actual behavior when cloning mocks (See #229)

---
+++ tests/functionals/classes/mock/controller.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace mageekguy\atoum\tests\functionals\mock;
+
+use mageekguy\atoum;
+
+require_once __DIR__ . '/../../runner.php';
+
+class timeTravel
+{
+    public function cloneAndModifyThisDate(\dateTime $date)
+    {
+        $this->startParallelTimeline($date)->modify('+1 day');
+    }
+
+    public function modifyThisDateWithoutCloning(\dateTime $date)
+    {
+        $date->modify('+1 day');
+    }
+
+    protected function startParallelTimeline(\dateTime $date)
+    {
+        return clone $date;
+    }
+}
+
+class controller extends atoum\tests\functionals\test\functional
+{
+    /** @tags issue issue-229 mock */
+    public function testCloneMock()
+    {
+        $this
+            ->given(
+                $date = new \mock\DateTime(),
+                $foo = new timeTravel()
+            )
+            ->if($foo->cloneAndModifyThisDate($date))
+            ->then
+                ->mock($date)->call('modify')->never()
+            ->if($foo->modifyThisDateWithoutCloning($date))
+            ->then
+                ->mock($date)->call('modify')->once()
+            ->if($foo->cloneAndModifyThisDate($date))
+            ->then
+                ->mock($date)->call('modify')->once()
+        ;
+    }
+}
---
+++ tests/functionals/test/functional.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace mageekguy\atoum\tests\functionals\test;
+
+use mageekguy\atoum;
+
+class functional extends atoum\test
+{
+    public function getTestNamespace()
+    {
+        return '#(?:^|\\\)tests?\\\functionals?\\\#i';
+    }
+}
