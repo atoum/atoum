6bd44a096c6589dfdccc2c51341322976bd4b37a
jubianchi <contact@jubianchi.fr>
2015-08-27 23:36:12 +0200

    Fix tests

--- tests/units/classes/scripts/git/pusher.php
+++ tests/units/classes/scripts/git/pusher.php
@@ -179,7 +179,7 @@ class pusher extends atoum
 						->once()
 					->call('tagVersion')
 						->before($this->mock($git)
-							->call('addAllAndCommit')->withArguments('Set version to DEVELOPMENT-0.0.1.')->once())
+							->call('addAllAndCommit')->withArguments('Set version to dev-master.')->once())
 						->after($this->mock($taggerEngine)->call('setSrcDirectory')->withArguments($pusher->getWorkingDirectory())->once())
 						->after($this->mock($taggerEngine)->call('setVersion')->withArguments('$Rev:' . ' DEVELOPMENT-0.0.1 $')->once()) // Don't remove concatenation operator to avoid tagger replace the string.
 							->once()
