d4d54fc1df0a998e6b61a80ae06fe8af5c108c71
jubianchi <contact@jubianchi.fr>
2017-02-14 21:23:27 +0100

    Enable style CI

--- resources/templates/test/generator/testClass.php
+++ resources/templates/test/generator/testClass.php
@@ -2,16 +2,14 @@
 
 namespace <tpl:testClassNamespace />;
 
-use
-	mageekguy\atoum,
-	<tpl:fullyQualifiedTestClassName /> as testedClass
-;
+use mageekguy\atoum;
+use <tpl:fullyQualifiedTestClassName /> as testedClass;
 
 <tpl:requireRunner>require_once __DIR__ . '/<tpl:relativeRunnerPath />';</tpl:requireRunner>
 
 class <tpl:testClassName /> extends atoum\test
 {<tpl:testMethods><tpl:testMethod>
-	public function test<tpl:methodName/>()
-	{
-	}
+    public function test<tpl:methodName/>()
+    {
+    }
 </tpl:testMethod></tpl:testMethods>}
