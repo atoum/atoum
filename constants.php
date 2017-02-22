1998c0be37e6a6186902c5f98b4d7be0e7361cc2
jubianchi <contact@jubianchi.fr>
2017-02-22 14:05:40 +0100

    Set version to dev-master.

--- constants.php
+++ constants.php
@@ -5,7 +5,7 @@ namespace mageekguy\atoum;
 if (defined(__NAMESPACE__ . '\running') === false) {
     define(__NAMESPACE__ . '\running', true);
     define(__NAMESPACE__ . '\directory', __DIR__);
-    define(__NAMESPACE__ . '\version', preg_replace('/\$Rev: ([^ ]+) \$/', '$1', '$Rev: 3.0.0 $'));
+    define(__NAMESPACE__ . '\version', preg_replace('/\$Rev: ([^ ]+) \$/', '$1', '$Rev: dev-master $'));
     define(__NAMESPACE__ . '\author', 'Frédéric Hardy');
     define(__NAMESPACE__ . '\mail', 'support@atoum.org');
     define(__NAMESPACE__ . '\repository', 'http://www.atoum.org/atoum');
