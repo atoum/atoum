42923f4fc3b351aec3a6c5d27cd40e8ef962efbc
Fabien Villepinte <fabien.villepinte@gmail.com>
2021-10-10 12:08:51 +0000

    Don't pass null to preg_replace()

--- classes/iterators/recursives/atoum/source.php
+++ classes/iterators/recursives/atoum/source.php
@@ -46,7 +46,7 @@ class source implements \outerIterator
     #[\ReturnTypeWillChange]
     public function key()
     {
-        return ($this->pharDirectory === null ? $this->innerIterator->key() : (preg_replace('#^(:[^:]+://)?' . preg_quote($this->sourceDirectory, '#') . '#', $this->pharDirectory, $this->innerIterator->current()) ?: null));
+        return ($this->pharDirectory === null ? $this->innerIterator->key() : (preg_replace('#^(:[^:]+://)?' . preg_quote($this->sourceDirectory, '#') . '#', $this->pharDirectory, $this->innerIterator->current() ?: '') ?: null));
     }
 
     #[\ReturnTypeWillChange]
